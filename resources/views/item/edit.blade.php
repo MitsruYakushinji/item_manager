11c69896d0a2244f0c3a12658b7df87aa4fec536
MitsruYakushinji <156178833+MitsruYakushinji@users.noreply.github.com>
2024-04-03 19:52:29 +0900

    firstCommit

---
+++ resources/views/item/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>商品編集ページ</title>
+</head>
+<body>
+    <h1>商品編集ページ</h1>
+</body>
+</html>
